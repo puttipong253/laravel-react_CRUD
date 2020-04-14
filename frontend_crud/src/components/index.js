cbf9e32058de95b384e6ae0c9d580970b3c4247d
PHUTHIPHONG MONGKHON <james_apple@hotmail.co.th>
2020-04-14 19:23:00 +0700

    add isLoading

--- frontend_crud/src/components/index.js
+++ frontend_crud/src/components/index.js
@@ -4,11 +4,13 @@ import Axios from '../api'
 export default function Index(props) {
     
     const [data, setData] = useState([])
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         Axios.get(`api/customers/`)
             .then(res =>{
                 setData(res.data)
+                setLoading(false)
             })
     }, [])
 
@@ -32,18 +34,37 @@ export default function Index(props) {
 
     return (
         <div>
-            {data.map(data => (
-                <table>
-                    <tbody>
-                        <tr key={data.id}>
-                            <td>{data.firstname}</td>
-                            <td><button onClick={() => onUpdate(data.id)}>edit</button></td>
-                            <td><button onClick={() => { if (window.confirm('Are you sure you want delete this item?')) onRemove(data.id)} }>Delete</button></td>
-                        </tr>
-                    </tbody>
-                </table>
-            ))}
-            <button onClick={onCreate}>Create</button>
+            {loading ? ("Loading...") : (
+                <div>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>ชื่อ</th>
+                                <th>นามสกุล</th>
+                                <th>อายุ</th>
+                                <th>อีเมล</th>
+                                <th>เบอร์โทรศัพท์</th>
+                                <th>แก้ไข</th>
+                                <th>ลบ</th>
+                            </tr>
+                        </thead>
+                            {data.map(data => (
+                            <tbody>
+                                <tr key={data.id}>
+                                    <td>{data.firstname}</td>
+                                    <td>{data.lastname}</td>
+                                    <td>{data.age}</td>
+                                    <td>{data.email}</td>
+                                    <td>{data.phonenumber}</td>
+                                    <td><button onClick={() => onUpdate(data.id)}>edit</button></td>
+                                    <td><button onClick={() => { if (window.confirm('Are you sure you want delete this item?')) onRemove(data.id)} }>Delete</button></td>
+                                </tr>
+                            </tbody>
+                        ))}
+                    </table>
+                    <button onClick={onCreate}>Create</button> 
+                </div>
+            )}
         </div>
     )
 }
